56dabaac960db307e6aafaae694abf31a86a5d6a
Fredrik Lundin <fredrik.lundin@capgemini.com>
2017-03-18 15:27:35 +0100

    refactor(init): setting main project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './core/app.component';
+import { CurrencyRateComparerComponent } from './components/currency-rate-comparer/currency-rate-comparer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyRateComparerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'crc-root',
+  template: `<p>App works!</p>`,
+})
+export class AppComponent {}
