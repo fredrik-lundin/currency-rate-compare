f0038e184df71428b953af445a1d950e6cf440a0
Fredrik Lundin <fredrik.lundin@capgemini.com>
2017-03-18 20:10:03 +0100

    test(CurrencyService): add test getCurrencyData

--- src/app/shared/services/currency-service.service.spec.ts
+++ src/app/shared/services/currency-service.service.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { CurrencyRate } from '../models/currency-rate.model';
+import { TestBed, inject, async } from '@angular/core/testing';
 import { HttpModule } from '@angular/http';
 
 import { CustomAmount } from '../models/custom-amount.model';
 import { CurrencyService } from './currency-service.service';
 
-describe('CurrencyServiceService - calculateCustomAmount', () => {
+describe('CurrencyServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpModule],
@@ -12,31 +13,45 @@ describe('CurrencyServiceService - calculateCustomAmount', () => {
     });
   });
 
-  it('should calculate correctly when source changed', inject([CurrencyService], (service: CurrencyService) => {
-    const oldCustomAmount: CustomAmount = {
-      sourceAmount: 10,
-      targetAmount: 10
-    };
-    const expected: CustomAmount = {
-      sourceAmount: 10,
-      targetAmount: 15
-    };
-    const actual = service.calculateCustomAmount(oldCustomAmount, true, 1.5);
+  describe('calculateCustomAmount', () => {
+    it('should calculate correctly when source changed', inject([CurrencyService], (service: CurrencyService) => {
+      const oldCustomAmount: CustomAmount = {
+        sourceAmount: 10,
+        targetAmount: 10
+      };
+      const expected: CustomAmount = {
+        sourceAmount: 10,
+        targetAmount: 15
+      };
+      const actual = service.calculateCustomAmount(oldCustomAmount, true, 1.5);
 
-    expect(expected).toEqual(actual);
-  }));
+      expect(expected).toEqual(actual);
+    }));
 
-  it('should calculate correctly when target changed', inject([CurrencyService], (service: CurrencyService) => {
-    const oldCustomAmount: CustomAmount = {
-      sourceAmount: 15,
-      targetAmount: 15
-    };
-    const expected: CustomAmount = {
-      sourceAmount: 10,
-      targetAmount: 15
-    };
-    const actual = service.calculateCustomAmount(oldCustomAmount, false, 1.5);
+    it('should calculate correctly when target changed', inject([CurrencyService], (service: CurrencyService) => {
+      const oldCustomAmount: CustomAmount = {
+        sourceAmount: 15,
+        targetAmount: 15
+      };
+      const expected: CustomAmount = {
+        sourceAmount: 10,
+        targetAmount: 15
+      };
+      const actual = service.calculateCustomAmount(oldCustomAmount, false, 1.5);
 
-    expect(expected).toEqual(actual);
-  }));
+      expect(expected).toEqual(actual);
+    }));
+  });
+
+  describe('getCurrencyData', () => {
+    it('should receive and map some data from the api', async(inject([CurrencyService], (service: CurrencyService) => {
+        service.getCurrencyData('NOK', 'SEK').subscribe(
+          res => {
+            expect(res.rate).toEqual(jasmine.any(Number));
+            expect(res.source).toEqual('NOK');
+            expect(res.target).toEqual('SEK');
+          }
+        );
+      })));
+  });
 });
